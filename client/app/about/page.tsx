a51d6e1dbe98ebe5b30827b0aed2ab87bde2fcbf
Ibrahim <ibrahim.diallo@edu.ece.fr>
2024-10-16 10:26:55 +0200

    feat: added tailwindcss to the project

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -1,9 +1,10 @@
+import '../../styles/globals.css';
+
 export default function About() {
     return (
-      <div>
-        <h2>About Me</h2>
-        <p>This is the about page where I talk about myself and my blog.</p>
+      <div className="p-4">
+        <h2 className="text-2xl font-bold mb-2">About Me</h2>
+        <p className="text-base text-gray-700 italic">This is the about page where I talk about myself and my blog.</p>
       </div>
     );
-  }
-  
+}
