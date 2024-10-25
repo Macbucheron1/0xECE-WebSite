b4c07990a0db8c10973eb82899ab87fb0fc222b7
mac <nathandeprat@hotmail.fr>
2024-10-25 15:58:40 +0200

    fix: is it still hidden?

--- client/app/components/AppFooter.tsx
+++ client/app/components/AppFooter.tsx
@@ -2,8 +2,10 @@ import "../../styles/globals.css"
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 p-4 text-center">
-      <p className="text-white text-sm">&copy; 2024 WebTech-101. All rights reserved.</p>
+    <footer className="bg-gray-800 p-4 text-center pb-[env(safe-area-inset-bottom)]">
+      <p className="text-white text-sm">
+        &copy; 2024 WebTech-101. All rights reserved.
+      </p>
     </footer>
   );
 }
