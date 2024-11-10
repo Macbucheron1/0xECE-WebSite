07a65ffbe535b7b873403e52b0eaeea49955d0ab
Ibrahim <ibrahim.diallo@edu.ece.fr>
2024-11-10 17:38:24 +0100

    feat: links "comments" to supabase et enhance style

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -278,7 +278,6 @@ export default function Home() {
                           La limite de 20 caractères est atteinte.
                         </p>
                       )}
-                      <p className="p-gray">{200 - name.length} caractères restants.</p>
                     </div>
                     <div className="mb-4">
                       <label className="block p-gray mb-2">Témoignage</label>
